317b4dcbfc04dcb6e7fd654baef7cb82a6033364
suraj <spro71612@gmail.com>
2025-08-04 14:26:52 +0545

    integrated collaborative editor using liveblocks yjs provider

--- src/components/Document.tsx
+++ src/components/Document.tsx
@@ -6,6 +6,7 @@ import { Button } from "./ui/button";
 import { doc, updateDoc } from "firebase/firestore";
 import { db } from "../../firebase";
 import { useDocumentData } from "react-firebase-hooks/firestore";
+import Editor from "./Editor";
 
 const Document = ({ id }: { id: string }) => {
   const [data, loading, error] = useDocumentData(doc(db, "documents", id));
@@ -54,7 +55,10 @@ const Document = ({ id }: { id: string }) => {
         {/* Avatars */}
       </div>
 
+      <hr className="pb-10" />
+
       {/* Collaborative Editor */}
+      <Editor />
     </div>
   );
 };
