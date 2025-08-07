6e568db9c59010efdf9f382c078d921de2dd2b25
suraj <spro71612@gmail.com>
2025-08-07 10:27:32 +0545

    smal change in service-key.json file

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -1,19 +1,20 @@
-import { initializeApp, getApps, App, getApp, cert } from "firebase-admin/app";
-
+import { initializeApp, cert, getApps, getApp, App } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-
-import serviceKey from "./service_key.json" assert { type: "json" };
 import { ServiceAccount } from "firebase-admin";
+
+// Import the service key JSON from root directory
+import serviceAccount from "./service_key.json";
 
 let app: App;
 
 if (getApps().length === 0) {
   app = initializeApp({
-    credential: cert(serviceKey as ServiceAccount),
+    credential: cert(serviceAccount as ServiceAccount),
   });
 } else {
   app = getApp();
 }
 
 const adminDb = getFirestore(app);
+
 export { app as adminApp, adminDb };
